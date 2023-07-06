38ffa608fda46dedb4919b6409b211039d9b46e9
pjh9139 <pjh9139@gmail.com>
2023-07-06 16:07:00 +0900

    FBV로 블로그 포스트 상세페이지 만들기"

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,6 @@ from .models import Post
 
 
 def index(request):
-
     posts = Post.objects.all().order_by('-pk')
 
     return render(
@@ -11,5 +10,17 @@ def index(request):
         'blog/index.html',
         {
             'posts': posts,
+        }
+    )
+
+
+def single_post_page(request, pk):
+    post = Post.objects.get(pk=pk)
+
+    return render(
+        request,
+        'blog/single_post_page.html',
+        {
+            'post': post,
         }
     )
